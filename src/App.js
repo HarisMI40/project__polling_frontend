03f7664a8d8776047f4e9dbf8e6a55b6d641a013
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2022-09-02 22:59:08 +0700

    rubah url halaman untuk root menjadi ke create

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           element={
             <Layout>
               {" "}
-              <AllPoll />{" "}
+              <CreatePoll />
             </Layout>
           }
         />
